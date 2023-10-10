1d48f82d1f42575056563f80e5b1591c0be9e184
sliznyaa <misterslaim122@gmail.com>
2023-10-10 20:38:04 +0300

    add new system out println

--- src/GitLesson04.java
+++ src/GitLesson04.java
@@ -1,5 +1,6 @@
 public class GitLesson04 {
     public static void main (String []args){
         System.out.println("Hello world from Git! I am Danil");
+        System.out.println("second commit to git");
     }
 }
